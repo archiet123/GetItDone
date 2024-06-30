d737d6aec71b257c15ff3a0aad04df3846354cb1
QIArchie <150247123+QIArchie@users.noreply.github.com>
2024-06-30 21:42:02 +0100

    prisma API post setup

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,12 +1,31 @@
-import { register } from "../../utils/auth.server";
+"use client";
+
+import { register } from "../utils/auth.server";
 import Image from "next/image";
 import { Select, Button } from "@chakra-ui/react";
+import { createTRPCClient, httpBatchLink } from "@trpc/client";
+import type { AppRouter } from "../server/index";
+import { User } from "../types/User";
+
+const trpc = createTRPCClient<AppRouter>({
+  links: [
+    httpBatchLink({
+      url: "http://localhost:3000",
+    }),
+  ],
+});
+
+const FetchUsers = async () => {
+  const User = trpc.userList;
+};
+
+const createdUser = await trpc.userCreate.mutate({ name: "sachinraja" });
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>hello world</p>
-      <Button onClick={(e: any) => register()}>hi</Button>
+      <p></p>
+      <Button onClick={FetchUsers}>hi</Button>
     </main>
   );
 }
--- script.ts
+++ script.ts
@@ -1,15 +1,15 @@
-// import { PrismaClient } from "@prisma/client";
-// const prisma = new PrismaClient();
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
-// async function Main() {
-//   const user = await prisma.user.findMany();
-//   console.log(user);
-// }
+async function Main() {
+  const user = await prisma.user.findMany();
+  console.log(user);
+}
 
-// Main()
-//   .catch((e) => {
-//     console.error(e.message);
-//   })
-//   .finally(async () => {
-//     await prisma.$disconnect();
-//   });
+Main()
+  .catch((e) => {
+    console.error(e.message);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
